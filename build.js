c80943be877169135f343ee8da1a512ac8a7f85e
Pásztor Dániel <danim1130@gmail.com>
2023-07-11 14:42:26 +0200

    Update build workflow

--- build.js
+++ build.js
@@ -3,9 +3,7 @@ const archiver = require('archiver');
 const lockfile = require('./package-lock-grid.json');
 const modules = fs.readdirSync('node_modules');
 const output = fs.createWriteStream('plugin-active-win.zip');
-const archive = archiver('zip', { zlib: { level: 1 } });
-
-archive.pipe(output);
+const archive = archiver('zip', { zlib: { level: 9 } });
 
 // Remove dependencies from node_modules
 for (const dependency in lockfile.dependencies) {
@@ -14,14 +12,28 @@ for (const dependency in lockfile.dependencies) {
   }
 }
 
-// Exclude files and directories from the archive
-const excludedFiles = ['archive.js', 'build.js', 'package-lock-grid.json', '.github'];
-for (const file of excludedFiles) {
-  archive.glob(`!${file}`, { cwd: '.', dot: true });
+const subfolder = 'my-project-files';
+if (!fs.existsSync(subfolder)) {
+  fs.mkdirSync(subfolder);
 }
 
-// Add remaining files to the archive
-archive.directory('.', false);
+const excludedFiles = [subfolder, 'archive.js', 'build.js', 'package-lock-grid.json', '.github', '.git'];
+
+// Get all files and directories in the current folder
+const files = fs.readdirSync('.');
+for (const file of files) {
+  // Exclude the excluded files/directories
+  if (!excludedFiles.includes(file)) {
+	fs.renameSync(file, `${subfolder}/${file}`);
+  }
+}
+
+output.on('close', () => {
+  console.log('Archive created successfully.');
+});
+
+archive.pipe(output);
+archive.directory(subfolder, false);
 archive.finalize();
 
 console.log('Dependencies removed and archive created.');
