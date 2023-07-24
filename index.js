fb83007c77c6c3cbed8e4312f33c814c1716bdcb
Pásztor Dániel <danim1130@users.noreply.github.com>
2023-07-24 16:07:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const activeWindow = require('active-win');
+const { askForScreenCaptureAccess } = require('node-mac-permissions')
 
 let interval = 1000
 let pageActivatorCriteria_0 = ''
@@ -18,6 +19,7 @@ exports.loadPlugin = async function(gridController, persistedData) {
     pageActivatorCriteria_2 = persistedData?.pageActivatorCriteria_2 ?? 'Firefox'
     pageActivatorCriteria_3 = persistedData?.pageActivatorCriteria_3 ?? ''
     isEnabled = true
+    askForScreenCaptureAccess()
     runLoop()
     
     return true
