f65fc19dc55c8b3edbe9aaa858ed72cad63544b8
Pásztor Dániel <danim1130@users.noreply.github.com>
2023-07-26 15:59:58 +0200

    Update build.js

--- build.js
+++ build.js
@@ -1,23 +1,15 @@
 const fs = require('fs');
 const archiver = require('archiver');
-const lockfile = require('./package-lock-grid.json');
 const modules = fs.readdirSync('node_modules');
 const output = fs.createWriteStream('plugin-active-win.zip');
 const archive = archiver('zip', { zlib: { level: 9 } });
-
-// Remove dependencies from node_modules
-/*for (const dependency in lockfile.dependencies) {
-  if (modules.includes(dependency)) {
-    fs.rmSync(`node_modules/${dependency}`, { recursive: true, force: true });
-  }
-}*/
 
 const subfolder = 'my-project-files';
 if (!fs.existsSync(subfolder)) {
   fs.mkdirSync(subfolder);
 }
 
-const excludedFiles = [subfolder, 'archive.js', 'build.js', 'package-lock-grid.json', '.github', '.git'];
+const excludedFiles = [subfolder, 'archive.js', 'build.js', '.github', '.git'];
 
 // Get all files and directories in the current folder
 const files = fs.readdirSync('.');
